92c018c8d8b54943ebad2fa81b4a239aa42b2202
sarani dinethma <saranidinethma@gmail.com>
2025-03-28 14:07:46 +0530

    update main

--- src/main/java/com/example/ticketingsystem/TicketingsystemApplication.java
+++ src/main/java/com/example/ticketingsystem/TicketingsystemApplication.java
@@ -2,12 +2,13 @@ package com.example.ticketingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Map;
 
 @SpringBootApplication
 public class TicketingsystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TicketingsystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(TicketingsystemApplication.class);
+        app.setDefaultProperties(Map.of("server.port", System.getenv("PORT") != null ? System.getenv("PORT") : "8080"));
+        app.run(args);
+    }
 }
