8a89bfda1b4138765f0d4ba87dfbe0cb0635d695
sarani dinethma <saranidinethma@gmail.com>
2025-03-28 14:10:08 +0530

    update Main

--- src/main/java/com/example/ticketingsystem/TicketingsystemApplication.java
+++ src/main/java/com/example/ticketingsystem/TicketingsystemApplication.java
@@ -2,13 +2,12 @@ package com.example.ticketingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Map;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class TicketingsystemApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(TicketingsystemApplication.class);
-        app.setDefaultProperties(Map.of("server.port", System.getenv("PORT") != null ? System.getenv("PORT") : "8080"));
-        app.run(args);
+        SpringApplication.run(TicketingsystemApplication.class, args);
     }
 }
